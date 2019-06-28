9131254e7f9a74abcb1aa4abbb828da178a0ccb7
Ridwan Balogun <balogunridwan@gmail.com>
2019-06-28 18:07:40 +0100

    Change folder structure

---
+++ src/Components/Button.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Button extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>App Component</h1>
+      </div>
+    );
+  }
+  
+}
+
+export default Button;
